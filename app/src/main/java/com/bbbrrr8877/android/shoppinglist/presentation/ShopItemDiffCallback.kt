f28fd6cdbeb2226ab5dd3065b672d4dccbfdc11b
Barbusya <bbbrrr8877@gmail.com>
2022-07-29 13:34:48 +0300

    DiffUtil ListAdapter

---
+++ app/src/main/java/com/bbbrrr8877/android/shoppinglist/presentation/ShopItemDiffCallback.kt
@@ -0,0 +1,14 @@
+package com.bbbrrr8877.android.shoppinglist.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.bbbrrr8877.android.shoppinglist.domain.ShopItem
+
+class ShopItemDiffCallback: DiffUtil.ItemCallback<ShopItem>() {
+    override fun areItemsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
+        return oldItem == newItem
+    }
+}
